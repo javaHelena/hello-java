18a3a64130a61b97b603d59b87e9da7c3ae509ea
Helena Hjertén <helena@hjerten.com>
2024-08-22 14:27:23 +0200

    Spring Initializr forst commit

---
+++ src/test/java/com/javahelena/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javahelena.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
