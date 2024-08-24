b934a14aa9e34badee28ecd9e7777b2ad973bc41
Helena Hjertén <helena@hjerten.com>
2024-08-24 20:52:36 +0200

    ADded hello controller

---
+++ src/main/java/com/javahelena/hello/api/HelloController.java
@@ -0,0 +1,21 @@
+package com.javahelena.hello.api;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @Value("${HELLO_MESSAGE:Hello World from the javaclass!}")
+    private String message;
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        log.info("Saying hello");
+        return message;
+    }
+}
