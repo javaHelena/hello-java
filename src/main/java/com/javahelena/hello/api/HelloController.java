97b814941c83ac56b086d0f5c0e7efd0006c0346
Helena Hjertén <helena@hjerten.com>
2024-08-26 23:21:55 +0200

    Change

--- src/main/java/com/javahelena/hello/api/HelloController.java
+++ src/main/java/com/javahelena/hello/api/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @Value("${HELLO_MESSAGE:Hello World from the javaclass again!}")
+    @Value("${HELLO_MESSAGE:Hello World from the javaclass!}")
     private String message;
 
     @GetMapping("/hello")
